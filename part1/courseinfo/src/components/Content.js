2e920ec0f06689482d005123026321dfe08334c0
Antonio José Leal Ferrández <ajleal@atsistemas.com>
2023-11-30 23:40:22 +0100

    1.3 course information, step3

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -4,9 +4,9 @@ import Part from '../components/Part'
 const Content = (props) => {
     return (
         <div> 
-            <Part part={props.part1} excercises={props.exercises1} />
-            <Part part={props.part2} excercises={props.exercises2} />
-            <Part part={props.part3} excercises={props.exercises3} />
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
         </div>
     );
 }
