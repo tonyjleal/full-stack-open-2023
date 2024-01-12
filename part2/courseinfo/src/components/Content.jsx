fb31e1b56a46f8dcb24038f8c82e6429e85dc524
Antonio José Leal Ferrández <ajleal@atsistemas.com>
2024-01-12 15:01:50 +0100

    2.2: course information, step 7

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
 import Part from "./Part"
 
 const Content = ({parts}) => {
+    
+    let total = 0;
+    parts.forEach( part => total += part.exercises )
 
     return (
         <> 
         {
-            parts.map(part => <Part key={part.id} part={part} />)
-        }            
+            parts.map(
+                part => 
+                <Part key={part.id} part={part} />
+            )
+        }        
+        <strong>total of {total} exercises</strong>            
         </>
     )
 }
