670c3f9ae024d27da4b1b18465716e4c7aa5d264
Antonio José Leal Ferrández <ajleal@atsistemas.com>
2024-01-12 19:49:11 +0100

    2.3: course information, step 8

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react"
 import Part from "./Part"
 
-const Content = ({parts}) => {
-    
-    let total = 0;
-    parts.forEach( part => total += part.exercises )
+const Content = ({ parts }) => {
+
+
+    const total = parts.reduce((accumulator, part) =>  accumulator + part.exercises, 0)
 
     return (
-        <> 
-        {
-            parts.map(
-                part => 
-                <Part key={part.id} part={part} />
-            )
-        }        
-        <strong>total of {total} exercises</strong>            
+        <>
+            {
+                parts.map(
+                    part =>
+                        <Part key={part.id} part={part} />
+                )
+            }
+            <strong>total of {total} exercises</strong>
         </>
     )
 }
