5e68639666451f5e40d2ae06cdd7f435539266c3
Antonio José Leal Ferrández <ajleal@atsistemas.com>
2024-01-30 13:08:27 +0100

    2.11: The phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [personsToShow, setPersonsToShow ] = useState(persons)
+  const [persons, setPersons ] = useState([]) 
+  const [personsToShow, setPersonsToShow ] = useState([])
   const [ filterName, setFilterName] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)        
+        setPersonsToShow(response.data)
+      })
+  }, [])
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
