531f13cd41514a22f5f70822acae97b0e66c17d3
Antonio José Leal Ferrández <tonyjleal@gmail.com>
2024-02-16 16:37:43 +0100

    3.2: Phonebook backend, step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,14 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const infoMessage = `
+                  <p>Phonebook has info for ${persons.length} people</p>
+                  <p>${new Date()}</p>
+                  `
+  response.send(infoMessage)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
