e89fa27d8b299f625d9fb2ac6cb457af31ddd22b
Antonio José Leal Ferrández <tonyjleal@gmail.com>
2024-02-20 22:43:39 +0100

    3.3: Phonebook backend, step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,19 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if(person) {
+    response.json(person)
+  }  else {
+    response.status(404).end()
+  }
+
+})
+
+
 app.get('/info', (request, response) => {
   const infoMessage = `
                   <p>Phonebook has info for ${persons.length} people</p>
