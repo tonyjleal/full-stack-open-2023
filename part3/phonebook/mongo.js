3ba9568d346418c5ca21aff264fefc086f446c21
Antonio José Leal Ferrández <tonyjleal@gmail.com>
2024-03-09 16:39:13 +0100

    3.12: Command-line database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.ebkhkhz.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3) {
+    Person.find({}).then( result => {
+        console.log('phonebook: ')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        });
+        mongoose.connection.close()
+    })
+} else {
+
+    const [, , , ...infoPerson] = process.argv;
+    const [name, number] = infoPerson;
+
+    const person = new Person({name, number})
+
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
