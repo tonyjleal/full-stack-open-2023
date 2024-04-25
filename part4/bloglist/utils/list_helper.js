108cc2e927903a67b607e06a68e4e8bceb0086f7
Antonio José Leal Ferrández <tonyjleal@gmail.com>
2024-04-26 00:07:53 +0200

    4.5* Helper Functions and Unit Tests, step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,20 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {}
+
+  const blogWithMaxLikes = blogs.reduce((max, blog) => (max.likes > blog.likes ? max : blog), blogs[0])
+
+  return {
+    title: blogWithMaxLikes.title,
+    author: blogWithMaxLikes.author,
+    likes: blogWithMaxLikes.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
