2819ad98612de3363d0e7b202d0e484842c6e6dd
Antonio José Leal Ferrández <tonyjleal@gmail.com>
2024-04-25 23:05:09 +0200

    4.4 Helper Functions and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
-  
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulator, currentBlog) => {
+    return accumulator + (currentBlog.likes || 0)
+  }, 0)
+}
+
 module.exports = {
-    dummy
-}   
+  dummy,
+  totalLikes
+}
